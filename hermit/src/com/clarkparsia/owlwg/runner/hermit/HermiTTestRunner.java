437becf9b378d76025f13904130232d4b96fd798
Birte Glimm <bglimm@Birtes-Mac.local>
2009-07-09 16:28:17 +0100

    HermiT update

--- hermit/src/com/clarkparsia/owlwg/runner/hermit/HermiTTestRunner.java
+++ hermit/src/com/clarkparsia/owlwg/runner/hermit/HermiTTestRunner.java
@@ -3,20 +3,37 @@ package com.clarkparsia.owlwg.runner.hermit;
 import java.net.URI;
 
 import org.semanticweb.HermiT.Configuration;
+import org.semanticweb.HermiT.EntailmentChecker;
 import org.semanticweb.HermiT.Reasoner;
-import org.semanticweb.owl.inference.OWLReasonerException;
-import org.semanticweb.owl.model.OWLAxiom;
-import org.semanticweb.owl.model.OWLOntology;
-import org.semanticweb.owl.model.OWLOntologyManager;
+import org.semanticweb.owlapi.inference.OWLReasonerException;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import com.clarkparsia.owlwg.Harness;
-import com.clarkparsia.owlwg.owlapi2.runner.impl.OwlApi2ReasonerTestRunner;
+import com.clarkparsia.owlwg.owlapi3.runner.impl.OwlApi3AbstractRunner;
+import com.clarkparsia.owlwg.owlapi3.testcase.impl.OwlApi3TestCaseFactory;
 
-public class HermiTTestRunner extends OwlApi2ReasonerTestRunner {
+public class HermiTTestRunner extends OwlApi3AbstractRunner {
 
-    public HermiTTestRunner() {
-        super(new Reasoner.ReasonerFactory(), URI
-                .create("http://hermit-reasoner.com/"));
+    private static final URI s_uri;
+
+    static {
+        s_uri = URI.create("http://hermit-reasoner.com/");
+    }
+
+    public String getName() {
+        return "HermiT";
+    }
+
+    public URI getURI() {
+        return s_uri;
+    }
+
+    @Override
+    protected boolean isConsistent(OWLOntologyManager manager, OWLOntology o)
+            throws OWLReasonerException {
+        Reasoner reasoner = new Reasoner(new Configuration(), manager, o);
+        return reasoner.isConsistent();
     }
 
     @Override
@@ -25,19 +42,18 @@ public class HermiTTestRunner extends OwlApi2ReasonerTestRunner {
             throws OWLReasonerException {
 
         Reasoner reasoner = new Reasoner(new Configuration(), manager, premise);
-        EntailmentChecker checker = new EntailmentChecker(reasoner, manager
-                .getOWLDataFactory());
-        for (OWLAxiom axiom : conclusion.getLogicalAxioms()) {
-            if (!checker.isEntailed(axiom))
-                return false;
-        }
-
-        return true;
+        EntailmentChecker checker = new EntailmentChecker(reasoner, manager.getOWLDataFactory());
+        return checker.entails(conclusion.getLogicalAxioms());
     }
 
-    public static void main(String[] args) {
-        System.setProperty(Harness.TEST_RUNNER_CLASS_PROPERTY, "com.clarkparsia.owlwg.runner.hermit.HermiTTestRunner");    
-        Harness.main(new String[] {"http://wiki.webont.org/exports/all.rdf"});
+    public static void main(String[] args) throws Exception {
+        System.setProperty( Harness.TEST_RUNNER_CLASS_PROPERTY, com.clarkparsia.owlwg.runner.hermit.HermiTTestRunner.class.getCanonicalName());
+        System.setProperty( Harness.TEST_FACTORY_CLASS_PROPERTY, OwlApi3TestCaseFactory.class.getCanonicalName() );
+        // http://wiki.webont.org/exports/approved/type-consistency.rdf
+        // http://wiki.webont.org/exports/all.rdf
+        // http://km.aifb.uni-karlsruhe.de/projects/owltests/index.php/Special:Ask/-5B-5B:New-2DFeature-2DDisjointObjectProperties-2D001-5D-5D/format%3Dowltest/title%3DNew-2DFeature-2DDisjointObjectProperties-2D001
+        System.out.println("Results will be written to: /Users/bglimm/Documents/workspace/owlwg-test/HermiT-results.txt");
+        Harness.main(new String[] { "http://km.aifb.uni-karlsruhe.de/projects/owltests/index.php/Special:Ask/-5B-5B:New-2DFeature-2DDisjointObjectProperties-2D001-5D-5D/format%3Dowltest/title%3DNew-2DFeature-2DDisjointObjectProperties-2D001", "-o/Users/bglimm/Documents/workspace/owlwg-test/HermiT-results2.txt", "-t300000" });
     }
-    
+
 }
